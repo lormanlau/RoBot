c3e36d9e0ee64b2a0f373c25fe75f524b4b9cbbf
Michael Cao <mike.x.cao@gmail.com>
2017-08-30 22:10:30 -0400

    fixed minor bugs

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,6 +2,6 @@
 exports.run = (bot, guild) => {
     bot.addServer(guild);
 	bot.sendServerCount(bot);
-    bot.user.setGame(config.prefix + 'help | ' + bot.guilds.size + ' Servers');
+    bot.user.setGame(bot.config.prefix + 'help | ' + bot.guilds.size + ' Servers');
     bot.joinleavehook('join', guild)
 }
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,6 +2,6 @@
 exports.run = (bot, guild) => {
     bot.removeServer(guild);
 	bot.sendServerCount(bot);
-    bot.user.setGame(config.PREFIX + 'help | ' + bot.guilds.size + ' Servers');
+    bot.user.setGame(bot.config.PREFIX + 'help | ' + bot.guilds.size + ' Servers');
     bot.joinleavehook('leave', guild)
 }
