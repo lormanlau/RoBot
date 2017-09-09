e2879d7743b4bf33abcf5790543ea0a1898b3e15
Michael Cao <mike.x.cao@gmail.com>
2017-09-09 18:52:12 -0400

    added jokes

---
+++ modules/bridge.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+	name: 'settings',
+	type: 'core',
+	usage: 'settings <arguments>',
+	permission: 4,
+	help: 'Changes server settings.',
+	main: function (bot, msg) {
+		
+	}
+};
---
+++ modules/tempmute.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'mute',
+	type: 'moderation',
+	usage: 'mute <user>',
+	permission: 2,
+	help: 'Mutes a specified user.',
+	main: function(bot, msg) {
+		var mutee = msg.mentions.users.array();
+		
+		for(var k = 0; k < mutee.length; k++) {
+            var str = msg.content.split(" ").splice(mutee.length).join(" ");
+            var ts = str.split(" ")[0]
+            var reason = str.split(" ").splice(1).join(" ");
+			var user = bot.users.get(mutee[k].id);
+			var member = msg.guild.members.get(mutee[k].id);
+            
+            if(member.hasPermission("ADMINISTRATOR"))
+                return msg.channel.send("I can't mute " + member + "!")
+            
+            
+			
+			msg.guild.channels.forEach(channel => {
+                if(channel.type == 'text')
+					channel.overwritePermissions(member, {SEND_MESSAGES: false})
+            })
+            
+			msg.reply(member + ' has been muted.')
+			
+			var mute = new Discord.RichEmbed()
+				.setColor(0xFF0000)
+				.setAuthor(user.username, user.avatarURL)
+				.addField('Member Muted', `**${user.username}#${user.discriminator} (${user.id}) was muted.**`)
+                .addField('Responsible Moderator', msg.member.displayName)
+                .addField('Duration', ts)
+				.addField('Reason', reason || "Not Specified")
+				.setFooter(`${msg.guild.name}`, `${msg.guild.iconURL}`)
+				.setTimestamp()
+			try {
+				var log = msg.guild.channels.find('name', 'mod-logs') || msg.guild.channels.find('name', 'modlogs');
+				log.send({embed: mute});
+			} catch (e) {
+				msg.channel.send({embed: mute});
+			}
+		}
+	}
+};
