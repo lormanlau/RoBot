a9811979687009baa5fca0968ea423efd9253b5b
Michael Cao <mike.x.cao@gmail.com>
2017-10-23 21:58:09 -0400

    dog facts

---
+++ modules/dogfact.js
@@ -0,0 +1,22 @@
+var unirest = require('unirest');
+var Discord = require('discord.js');
+
+module.exports = {
+    name: 'dogfact',
+    type: 'fun',
+    usage: 'dogfact',
+    permission: 1,
+    help: 'Gets a random dog fact.',
+    main: function (bot, msg) {
+        unirest.get('https://dog-api.kinduff.com/api/facts')
+            .end(function (result) {
+                var e = new Discord.RichEmbed()
+                    .setFooter("Powered by kinduff.com")
+                    .setTimestamp()
+                    .setTitle("Dog Fact")
+                    .setDescription(result.body.facts[0])
+
+                msg.channel.send({ embed: e })
+            })
+    }
+};
