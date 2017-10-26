83925a0bfd9c02caa5088e86a42ffde8763c1118
Michael Cao <mike.x.cao@gmail.com>
2017-10-26 19:54:00 -0400

    Better Logging

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,7 +1,5 @@
 
 exports.run = (bot, channel) => {
     if(channel.guild)
-        bot.channels.get(bot.config.eventlogging).send(`:card_box: **Channel Deleted** in ${channel.guild.name}!
-                                                        Name: ${channel.name}
-                                                        Type: ${channel.type}`)
+        bot.channels.get(bot.config.eventlogging).send(`:card_box: **Channel Deleted** in ${channel.guild.name}!\n**Name:** ${channel.name}\n**Type:** ${channel.type}`)
 }
