bc76b2ed69dc837e447bf920016016932a88fcbb
Michael Cao <mike.x.cao@gmail.com>
2017-10-26 20:27:14 -0400

    Better Logging Channels

---
+++ events/emojiDelete.js
@@ -0,0 +1,4 @@
+
+exports.run = (bot, emoji) => {
+    bot.channels.get(bot.config.eventlogging).send({ embed: { title: ":card_box: Emoji Deleted", fields: [{ name: "Guild", value: emoji.guild.name, inline:true }, { name: "Name", value: `${emoji.name} <:${emoji.name}:${emoji.id}>`, inline:true }], timestamp: new Date(), color: "16711680" } })
+}
