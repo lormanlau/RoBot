19f901731aae34dbd62f3d82bb9c9b4fa7dbeb04
Michael Cao <mike.x.cao@gmail.com>
2017-10-26 20:18:16 -0400

    Better Logging Channels

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -1,5 +1,5 @@
 
-exports.run = (bot, oldchannel, newChannel) => {
-    if (channel.guild && (oldChannel.name != newChannel.name || oldChannel.topic != newChannel.topic))
+exports.run = (bot, oldChannel, newChannel) => {
+    if (oldChannel.guild && (oldChannel.name != newChannel.name || oldChannel.topic != newChannel.topic))
         bot.channels.get(bot.config.eventlogging).send({ embed: { title: ":arrows_clockwise: Channel Updated", fields: [{ name: "Guild", value: channel.guild.name, inline:true }, { name: "Old Name", value: oldChannel.name, inline:true }, { name: "New Name", value: newChannel.name, inline:true }, { name: "Old Topic", value: oldChannel.topic, inline:true }, { name: "New Topic", value: newChannel.topic, inline:true }, { name: "Type:", value: channel.type, inline:true }], timestamp: new Date(), color: "65280" } })
 }
