d9a1cb3ba333aaa4ef93e0dfbae2759ff1894914
Michael Cao <mike.x.cao@gmail.com>
2017-10-26 20:03:38 -0400

    Better Logging Channels

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,6 +1,5 @@
 
 exports.run = (bot, channel) => {
     if (channel.guild)
-        msg.channel.send({ embed: { title: ":card_box: Channel Created", fields: [{ name: "Guild", "value": channel.guild.name}], "timestamp": new Date() } })
-    bot.channels.get(bot.config.eventlogging).send(`:card_box: **Channel Created** in ${channel.guild.name}!\n**Name:** ${channel.name}\n**Type:** ${channel.type}`)
+        bot.channels.get(bot.config.eventlogging).send({ embed: { title: ":card_box: Channel Created", fields: [{ name: "Guild", value: channel.guild.name }, { name: "Name", value: channel.name }, { name: "Type:", value: channel.type }], timestamp: new Date(), color: "#00FF00" } })
 }
