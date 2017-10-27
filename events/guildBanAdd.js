6a3fa6896e61e2fa1670a07d48e27425e17316aa
Michael Cao <mike.x.cao@gmail.com>
2017-10-26 21:56:29 -0400

    tba

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -8,21 +8,4 @@ exports.run = (bot, guild, user) => {
             
         }
     });
-
-    bot.channels.get(bot.config.eventlogging).send({
-        embed: {
-            title: ":card_box: Emoji Deleted",
-            fields: [{
-                name: "Guild",
-                value: emoji.guild.name,
-                inline: true
-            }, {
-                name: "Name",
-                value: `${emoji.name}`,
-                inline: true
-            }],
-            timestamp: new Date(),
-            color: "16711680"
-        }
-    })
 }
