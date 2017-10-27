d578a5e9b94ef1ce8da2e1e510e8a8cb24a59167
Michael Cao <mike.x.cao@gmail.com>
2017-10-26 20:01:43 -0400

    Better Logging

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,5 +1,6 @@
 
 exports.run = (bot, channel) => {
-    if(channel.guild)
-        bot.channels.get(bot.config.eventlogging).send(`:card_box: **Channel Created** in ${channel.guild.name}!\n**Name:** ${channel.name}\n**Type:** ${channel.type}`)
+    if (channel.guild)
+        msg.channel.send({ embed: { title: ":card_box: Channel Created", fields: [{ name: "Guild", "value": channel.guild.name}], "timestamp": new Date() } })
+    bot.channels.get(bot.config.eventlogging).send(`:card_box: **Channel Created** in ${channel.guild.name}!\n**Name:** ${channel.name}\n**Type:** ${channel.type}`)
 }
--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,5 +1,5 @@
 
 exports.run = (bot, channel) => {
-    if(channel.guild)
-        bot.channels.get(bot.config.eventlogging).send(`:card_box: **Channel Deleted** in ${channel.guild.name}!\n**Name:** ${channel.name}\n**Type:** ${channel.type}`)
+    if (channel.guild)
+        msg.channel.send({ embed: { title: ":card_box: Channel Deleted", fields: [{ name: "Guild", value: channel.guild.name }, { name: "Name", value: channel.name }, { name: "Type:", value: channel.type }], timestamp: new Date(), color: "#00FF00" } })
 }
