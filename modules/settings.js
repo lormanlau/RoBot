877a8752e97757d8112f65da0d0ac526581b96c4
Michael Cao <mike.x.cao@gmail.com>
2017-10-28 17:38:44 -0400

    Settings cleanup

--- modules/settings.js
+++ modules/settings.js
@@ -26,7 +26,7 @@ module.exports = {
 			bot.getAllSettings(msg.guild.id).then(obj => {
 				for (var key in obj) {
 					if (obj.hasOwnProperty(key) && key != "id" && key != "name" && key != "prefix") {
-						settings.addField(key, obj[key])
+						settings.addField(key, obj[key] || "None")
 					}
 				}
 				msg.channel.send({ embed: settings })
