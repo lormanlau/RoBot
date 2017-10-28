5fbb257bab237528918660a79ffdbfbf340a74e4
Michael Cao <mike.x.cao@gmail.com>
2017-10-28 15:09:09 -0400

    new settings

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,6 +1,6 @@
 
 exports.run = (bot, guild, user) => {
-    msg.guild.fetchAuditLogs({ options: { limit: 5 } }).then(logs => {
+    guild.fetchAuditLogs({ options: { limit: 5 } }).then(logs => {
         var action = logs.entries.array()[0]
         if (action.executor == bot.user && action.target == user) {
             return;
