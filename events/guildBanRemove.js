8a339f05524cba60d93a55595609818d4b29fd34
Michael Cao <mike.x.cao@gmail.com>
2017-10-28 18:11:10 -0400

    Settings cleanup

---
+++ events/guildBanRemove.js
@@ -0,0 +1,16 @@
+var Discord = require('discord.js')
+
+exports.run = (bot, guild, user) => {
+    guild.fetchAuditLogs({ options: { limit: 5 } }).then(logs => {
+        var action = logs.entries.array()[0]
+        var ban = new Discord.RichEmbed();
+        ban.setColor(0x00FF00)
+            .setAuthor(user.username, user.avatarURL)
+            .addField('Member Unbanned', `**${user.username}#${user.discriminator} (${user.id}) was unbanned from the server.**`)
+            .addField('Responsible Moderator', action.executor.username)
+            .addField('Reason', action.reason || 'Not Specified')
+            .setFooter(`${guild.name} | ${guild.members.size} members`, `${guild.iconURL}`)
+            .setTimestamp()
+        bot.channels.get('373256239318302721').send({ embed: ban });
+    });
+}
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,23 @@
+var Discord = require('discord.js')
+
+exports.run = (bot, member) => {
+    bot.getCurrentSetting('welcomeMessagesEnabled', member.guild.id).then(res => {
+        if(res == 1) {
+            bot.getCurrentSetting('welcomeMessage', member.guild.id).then(text => {
+                var welcome = new Discord.RichEmbed()
+                    .setAuthor(member.user.username, member.user.avatarURL)
+                    .setFooter(member.guild.name)
+                    .setTimestamp()
+                    .setColor("#00FF00")
+                text.replace('{server:name}', member.guild.name)
+                    .replace('{user:mention}', member.user)
+                    .replace('{server:membercount}', member.guild.members.size)
+
+                bot.getCurrentSetting('announcementChannel', member.guild.id).then(id => {
+                    var channel = member.guild.channels.get(id)
+
+                })
+            })
+        }
+    })
+}
