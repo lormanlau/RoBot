4fd5063593749a450e12947cb718b1f04a2bf91c
Michael Cao <mike.x.cao@gmail.com>
2017-12-22 19:33:52 -0500

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,5 +1,26 @@
-
 exports.run = (bot, channel) => {
-    if (channel.guild)
-        bot.channels.get(require('../config.json').eventlogging).send({ embed: { title: ":card_box: Channel Created", fields: [{ name: "Guild", value: channel.guild.name, inline:true }, { name: "Name", value: channel.name, inline:true }, { name: "Type:", value: channel.type, inline:true }], timestamp: new Date(), color: "65280" } })
+	if (channel.guild) {
+		setTimeout(function() {
+			bot.channels.get(require('../config.json').eventlogging).send({
+				embed: {
+					title: ":card_box: Channel Created",
+					fields: [{
+						name: "Guild",
+						value: channel.guild.name,
+						inline: true
+					}, {
+						name: "Name",
+						value: channel.name,
+						inline: true
+					}, {
+						name: "Type:",
+						value: channel.type,
+						inline: true
+					}],
+					timestamp: new Date(),
+					color: "65280"
+				}
+			})
+		}, 3000);
+	}
 }
