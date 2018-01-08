e62176b4cd4ea3e4bbff463354742ac7bb3b83f4
Michael Cao <mike.x.cao@gmail.com>
2018-01-08 17:17:58 -0500

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -151,7 +151,7 @@ module.exports = (bot) => {
 					}
 					if (msg.author.id == bot.config.owner) resolve(true);
 					if (msg.guild.members.get(bot.config.owner).hasPermission("MANAGE_MESSAGES")) resolve(true);
-					resolve(true); //TODO: Set to false on Stable
+					resolve(false); //TODO: Set to false on Stable
 				});
 		});
 	}
