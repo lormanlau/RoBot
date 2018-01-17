5c63bc2cade120c94aafed741bff3b3fe3f9f1b3
Michael Cao <mike.x.cao@gmail.com>
2018-01-16 23:44:09 -0500

    fixec kick command

--- modules/kick.js
+++ modules/kick.js
@@ -1,48 +1,49 @@
-//Work on permission checks in this command 
+// Work on permission checks in this command
 module.exports = {
-	name: 'kick',
-	type: 'moderation', 
-	usage: 'kick <usermention> <reason>',
-	permission: 3,
-	help: 'Kicks a specified user.',
-	main: function(bot, msg) {
-		const Discord = require("discord.js");
-		var banee = msg.mentions.users.array()[0];
+    name: 'kick',
+    type: 'moderation',
+    usage: 'kick <usermention> <reason>',
+    permission: 3,
+    help: 'Kicks a specified user and places it in mod logs.',
+    main: function(bot, msg) {
+        const Discord = require('discord.js');
+        var kickee = msg.mentions.users.array()[0];
 
-		if (msg.member.hasPermission('KICK_MEMBERS') || msg.member.hasPermission('ADMINISTRATOR')) {
-			try {
-				var kicked = msg.guild.members.get(banee.id);
-				var user = bot.users.get(banee.id);
-				var guild = msg.guild;
-				var reason = msg.content.split(" ").splice(1).join(" ");
-				
-				if(reason == "")
-					var reason = "Not specified.";
-				
-				kicked.kick(reason);
+        if (msg.member.hasPermission('KICK_MEMBERS') || msg.member.hasPermission('ADMINISTRATOR')) {
+            try {
+                var kicked = msg.guild.members.get(kickee.id);
+                var user = bot.users.get(kickee.id);
+                var guild = msg.guild;
+                var reason = msg.content.split(' ').splice(1).join(' ');
 
-				msg.reply(banee + " has been successfullly kicked.");
-				
-				var ban = new Discord.RichEmbed();
-				ban.setColor(0xFFB200)
+                if (reason === '') {
+                    reason = 'Not specified.';
+                }
+
+                kicked.kick(reason);
+
+                msg.reply(kickee + ' has been successfullly kicked.');
+
+                var ban = new Discord.RichEmbed();
+                ban.setColor(0xFFB200)
 					.setAuthor(user.username, user.avatarURL)
 					.addField('Member Kicked', `**${user.username}#${user.discriminator} (${user.id}) was kicked from the server.**`)
 					.addField('Responsible Moderator', msg.member.displayName)
 					.addField('Reason', reason)
 					.setFooter(`${guild.name} | ${guild.members.size} members`, `${guild.iconURL}`)
-					.setTimestamp()
+					.setTimestamp();
 
-				try {
-					var log = msg.guild.channels.find('name', 'mod-logs');
-					log.send({embed: ban});
-				} catch (e) {
-					msg.channel.send({embed: ban});
-				}
-			} catch (e) {
-				console.error(e);
-			}
-		} else {
-			msg.reply(" you do not have permission to perform this action!");
-		}
-	}
+                try {
+                    var log = msg.guild.channels.find('name', 'mod-logs');
+                    log.send({ embed: ban });
+                } catch (e) {
+                    msg.channel.send({ embed: ban });
+                }
+            } catch (e) {
+                console.error(e);
+            }
+        } else {
+            msg.reply(' you do not have permission to perform this action!');
+        }
+    },
 };
