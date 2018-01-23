f47b270d3d77699211467fb019f2fd5daa1a1672
Michael Cao <mike.x.cao@gmail.com>
2018-01-22 22:10:32 -0500

    sudosay improvements

--- modules/sudosay.js
+++ modules/sudosay.js
@@ -4,9 +4,25 @@ module.exports = {
     usage: 'sudosay',
     permission: 6,
     help: 'Makes the bot say something.',
-    main: function(bot, msg) {
+    main: function (bot, msg) {
         if (msg.author.id === require('../config.json').owner) {
-            msg.channel.send(msg.content);
+            var content = msg.content;
+            if (content.indexOf('mention:') > -1) {
+                if (content.indexOf(' ', content.indexOf('mention:') + 8) > -1) {
+                    var mention = content.substring(content.indexOf('mention:') + 8, content.indexOf(' ', content.indexOf('mention:') + 8));
+                } else {
+                    mention = content.substring(content.indexOf('mention:') + 8, content.length);
+                }
+                console.log(mention);
+                if (bot.users.get(mention)) {
+                    if (content.indexOf(' ', content.indexOf('mention:') + 8) > -1) {
+                        content = content.replace(content.substring(content.indexOf('mention:'), content.indexOf(' ', content.indexOf('mention:') + 8)), '<@' + bot.users.get(mention).id + '>');
+                    } else {
+                        content = content.replace(content.substring(content.indexOf('mention:'), content.length, '<@' + bot.users.get(mention).id + '>'));
+                    }
+                }
+            }
+            msg.channel.send(content);
             msg.delete();
         }
     },
