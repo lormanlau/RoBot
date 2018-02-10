99f2e98b73265f5adf1cbf0890c6f17f7471a5f5
Michael Cao <mike.x.cao@gmail.com>
2018-02-09 22:27:53 -0500

    avatar support?

--- modules/tba.js
+++ modules/tba.js
@@ -168,8 +168,23 @@ module.exports = {
             } else {
                 m.channel.sendMessage('Arguments for district subcommand: list');
             }
+        } else if (args === 'avatar') {
+            let year = m.content.split(' ')[2];
+            console.log(year);
+            if (year === undefined) { year = curYear; }
+            req.getTeamMedia(teamNumber, year).then(d => {
+                var found = false;
+                for (var i = 0; i < d.length; i++) {
+                    if (d[i].type === 'avatar') {
+                        found = true;
+                        m.channel.send('**Team ' + teamNumber + '\'s** avatar for the FIRST POWER UP season:',
+                            { files: [new Buffer(d.details.base64Image, 'base64')] });
+                    }
+                }
+                if (!found) m.channel.send('Unfortunately Team ' + teamNumber + ' does not have an avatar.');
+            });
         } else {
-            m.channel.sendMessage('Please specify an argument! Accepted arguments: team, awards, media, robots, events, district');
+            m.channel.sendMessage('Please specify an argument! Accepted arguments: team, avatar, awards, media, robots, events, district');
         }
 
         /* else if(subcommand == "events") {
