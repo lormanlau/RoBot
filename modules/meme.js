f130c6834164154735b94d2ae4aab10d7904ad46
Michael Cao <mike.x.cao@gmail.com>
2018-02-13 17:32:38 -0500

    memes

---
+++ modules/meme.js
@@ -0,0 +1,33 @@
+var unirest = require('unirest');
+
+module.exports = {
+    name: 'meme',
+    type: 'fun',
+    usage: 'meme',
+    permission: 1,
+    help: 'Get a meme',
+    main: function(bot, msg) {
+        unirest.get('https://api.weeb.sh/images/random?filetype=gif&type=discord_memes')
+            .headers({
+                Authorization: 'Wolke ' + bot.config.weebsh,
+            })
+            .end(res => {
+                console.log(res.body);
+                msg.channel.send({
+                    embed: {
+                        image: {
+                            url: res.body.url,
+                        },
+                        color: msg.guild.me.displayColor,
+                        footer: {
+                            text: 'Powered by weeb.sh',
+                        },
+                        title: {
+                            text: 'Discord Meme',
+                        },
+                        timestamp: new Date(),
+                    },
+                });
+            });
+    },
+};
