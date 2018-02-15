e9f30f8981f09ce2caf92931dbab604d3a50fc7a
Michael Cao <mike.x.cao@gmail.com>
2018-02-15 11:43:26 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 exports.run = bot => {
+    if (process.argv[2] && process.argv[2] === '--travis') process.exit(0);
+    
     bot.sendServerCount();
     bot.syncServers();
     bot.startGameCycle();
     bot.awaitConsoleInput();
 
     bot.log(`${bot.user.username} is online and ready to serve in ${bot.channels.size} channels on ${bot.guilds.size} servers!`);
-
-    if (process.argv[2] && process.argv[2] === '--travis') process.exit(0);
 };
