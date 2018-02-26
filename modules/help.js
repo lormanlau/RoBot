fcb213fbe5aa0a5e40d11e81dd95eb6a3059aa1a
Michael Cao <mike.x.cao@gmail.com>
2018-02-26 16:37:17 -0500

    updated help command

--- modules/help.js
+++ modules/help.js
@@ -5,53 +5,86 @@ module.exports = {
     usage: 'help <optional-arg>',
     permission: 1,
     help: 'Gives you the documentation for the bot\'s commands.',
-    main: function(bot, msg) {
+    main: function (bot, msg) {
         var r = Math.floor(Math.random() * 256),
             g = Math.floor(Math.random() * 256),
             b = Math.floor(Math.random() * 256);
-        bot.getPrefix(msg).then(prefix => {
-            if (!msg.args[0]) {
-                msg.channel.send(':mailbox: Check your DMs!');
-                const myCommands = bot.commands.filter(c => c.permission <= bot.permLevel(msg));
-                var help = new Discord.RichEmbed();
-                help.setTitle(bot.user.username + ' Command List')
-                    .setDescription(`Do ${prefix}help <commandname> for details`)
-                    .setColor([r, g, b]);
-                myCommands.forEach(c => {
-                    if (help.fields.length < 25) {
-                        help.addField(prefix + c.name, c.help, true);
-                    } else {
-                        msg.author.send({ embed: help });
-                        help = new Discord.RichEmbed()
-                            .setColor([r, g, b])
-                            .addField(prefix + c.name, c.help, true);
-                    }
-                });
-                help.setFooter(bot.user.username + ' Command List')
-                    .setTimestamp();
-                msg.author.send({ embed: help });
-                var invite = new Discord.RichEmbed()
-                    .setTitle(bot.user.username + ' Help Server')
-                    .setColor([r, g, b])
-                    .setDescription('Come join us at https://discord.gg/8QebTbk for support and more!')
-                    .setTimestamp();
-                msg.author.send({ embed: invite });
-            } else {
-                let command = msg.args[0];
-                if (bot.commands.has(command)) {
-                    command = bot.commands.get(command);
-                    var helpCommand = new Discord.RichEmbed();
-                    helpCommand.setTitle(command.name)
-                        .addField('Description', `${command.help}`)
-                        .addField('Usage', `${prefix}${command.usage}`)
+        if (!msg.args[0]) {
+            /*
+            msg.channel.send(':mailbox: Check your DMs!');
+            const myCommands = bot.commands.filter(c => c.permission <= bot.permLevel(msg));
+            var help = new Discord.RichEmbed();
+            help.setTitle(bot.user.username + ' Command List')
+                .setDescription(`Do ${prefix}help <commandname> for details`)
+                .setColor([r, g, b]);
+            myCommands.forEach(c => {
+                if (help.fields.length < 25) {
+                    help.addField(prefix + c.name, c.help, true);
+                } else {
+                    msg.author.send({ embed: help });
+                    help = new Discord.RichEmbed()
                         .setColor([r, g, b])
-                        .addField('Join our support server!', 'Come join us at https://discord.gg/8QebTbk for support and more!');
-                    /* if (command.conf.aliases != "") {
-                        helpCommand.addField('Aliases', `${command.conf.aliases.join(', ')}`)
-                    }*/
-                    msg.channel.send({ embed: helpCommand });
+                        .addField(prefix + c.name, c.help, true);
+                }
+            });
+            help.setFooter(bot.user.username + ' Command List')
+                .setTimestamp();
+            msg.author.send({ embed: help });
+            var invite = new Discord.RichEmbed()
+                .setTitle(bot.user.username + ' Help Server')
+                .setColor([r, g, b])
+                .setDescription('Come join us at https://discord.gg/8QebTbk for support and more!')
+                .setTimestamp();
+            msg.author.send({ embed: invite });
+            */
+            try {
+                const myCommands = bot.commands.filter(c => c.permission <= bot.permLevel(msg));
+                const commandNames = myCommands.keyArray();
+                const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+
+                let currentCategory = '';
+                let output = `= Command List =\n\n[Use ${msg.prefix}help <commandname> in a guild channel for details]\n`;
+                const sorted = myCommands.array().sort((p, c) => p.type > c.type ? 1 : p.name > c.name && p.type === c.type ? 1 : -1);
+                sorted.forEach(c => {
+                    const cat = toProperCase(c.type);
+                    if (currentCategory !== cat) {
+                        output += `\u200b\n== ${cat} ==\n`;
+                        currentCategory = cat;
+                    }
+                    output += `${msg.prefix}${c.name}${' '.repeat(longest - c.name.length)} :: ${c.help}\n`;
+                });
+                output += `Come join us at https://discord.gg/8QebTbk for support and more!`;
+                msg.channel.send('Please check your inbox for a list of my commands.');
+                msg.author.send(output, { code: 'asciidoc', split: { char: '\u200b' } });
+            } catch (error) {
+                if (error.message === 'Cannot send messages to this user') {
+                    msg.reply('I cannot send you the commands message, as it appears you have DMs disabled.');
+                } else {
+                    bot.error(error);
                 }
             }
-        });
+        } else {
+            let command = msg.args[0];
+            if (bot.commands.has(command)) {
+                command = bot.commands.get(command);
+                var helpCommand = new Discord.RichEmbed();
+                helpCommand.setTitle(command.name)
+                    .setFooter(bot.user.username)
+                    .setTimestamp()
+                    .addField('Category', `${toProperCase(command.type)}`)
+                    .addField('Description', `${command.help}`)
+                    .addField('Usage', `${msg.prefix}${command.usage}`)
+                    .setColor([r, g, b])
+                    .addField('Join our support server!', 'Come join us at https://discord.gg/8QebTbk for support and more!');
+                /* if (command.conf.aliases != "") {
+                    helpCommand.addField('Aliases', `${command.conf.aliases.join(', ')}`)
+                }*/
+                msg.channel.send({ embed: helpCommand });
+            }
+        }
+
+        function toProperCase(m) {
+            return m.replace(/([^\W_]+[^\s-]*) */g, function(txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        }
     },
 };
