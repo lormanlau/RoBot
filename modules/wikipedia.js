a85bdb64743d4c05896c97cd5eddad61db660caa
Michael Cao <mike.x.cao@gmail.com>
2018-02-28 16:28:50 -0500

    purge dep

--- modules/wikipedia.js
+++ modules/wikipedia.js
@@ -5,28 +5,20 @@ module.exports = {
     permission: 1,
     help: 'Fetches a Wikipedia article',
     main: async function(bot, msg) {
-        const wiki = require('wikipediajs'), { RichEmbed } = require('discord.js'), wiki2 = require('wikijs').default;
+        const { RichEmbed } = require('discord.js'), wiki = require('wikijs').default;
         msg.content = msg.content.replace(/ /g, '_');
-        wiki.search(msg.content)
-            .then(res => {
-                if (res.query) {
-                    res = res.query.pages[Object.keys(res.query.pages)[0]];
-                    wiki2().page(msg.content).then(async function(page) { // eslint-disable-line prefer-arrow-callback
-                        const info = await page.summary();
-                        console.log(info);
-                        const image = await page.mainImage();
-                        msg.channel.send({
-                            embed: new RichEmbed()
-                                .setAuthor('Wikipedia: ' + res.title, 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png', res.fullurl)
-                                .setFooter('Powered by wikipediajs')
-                                .setDescription(info.substring(0, info.indexOf('\n')))
-                                .setImage(image)
-                                .setTimestamp()
-                                .setColor(msg.guild.me.displayHexColor),
-                        });
-                    });
-                }
-            })
-            .catch(error => console.log(error));
+        wiki().page(msg.content).then(async function(page) { // eslint-disable-line prefer-arrow-callback
+            const info = await page.summary();
+            const image = await page.mainImage();
+            msg.channel.send({
+                embed: new RichEmbed()
+                    .setAuthor('Wikipedia: ' + page.raw.title, 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png', page.raw.fullurl)
+                    .setFooter('Powered by wikipediajs')
+                    .setDescription(info.substring(0, info.indexOf('\n')))
+                    .setImage(image)
+                    .setTimestamp()
+                    .setColor(msg.guild.me.displayHexColor),
+            });
+        });
     },
 };
