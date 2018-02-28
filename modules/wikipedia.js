81a158dc00adecba87e377caee6103244ee54a3d
Michael Cao <mike.x.cao@gmail.com>
2018-02-28 16:25:54 -0500

    Wiki Error

--- modules/wikipedia.js
+++ modules/wikipedia.js
@@ -13,12 +13,13 @@ module.exports = {
                     res = res.query.pages[Object.keys(res.query.pages)[0]];
                     wiki2().page(msg.content).then(async function(page) { // eslint-disable-line prefer-arrow-callback
                         const info = await page.summary();
+                        console.log(info);
                         const image = await page.mainImage();
                         msg.channel.send({
                             embed: new RichEmbed()
                                 .setAuthor('Wikipedia: ' + res.title, 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png', res.fullurl)
                                 .setFooter('Powered by wikipediajs')
-                                .setDescription(info.indexOf('\n'))
+                                .setDescription(info.substring(0, info.indexOf('\n')))
                                 .setImage(image)
                                 .setTimestamp()
                                 .setColor(msg.guild.me.displayHexColor),
