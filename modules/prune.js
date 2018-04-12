7aa2fa471e6299c6c4a17f8f2f14592aa6fbca21
Michael Cao <mike.x.cao@gmail.com>
2018-04-12 11:05:26 -0400

    prune peoples

--- modules/prune.js
+++ modules/prune.js
@@ -2,7 +2,7 @@
 module.exports = {
     name: 'prune',
     type: 'moderation',
-    usage: 'prune <amount>',
+    usage: 'prune <amount> | <user-mention> | bots',
     permission: 2,
     help: 'Prunes messages from the channel.',
     main: function(bot, msg) {
@@ -15,6 +15,19 @@ module.exports = {
 
                 msg.channel.send('Deleted ' + num + ' messages under request of <@' + msg.author.id + '>')
                     .then(msg2 => setTimeout(() => { msg2.delete(); }, 5000));
+            } else if (msg.mentions.users.first()) {
+                var amount = num.split(" ")[1] || null;
+                msg.channel.fetchMessages({
+					limit: 100
+				})
+				.then(messages => {
+					msgar = messages.array();
+					msgar = msgar.filter(msg => msg.author.id === msg.mentions.users.first().id);
+					if (amount && !isNaN(amount)) msgar.length = amount + 1;
+					msgar.map(msg => msg.delete().catch(console.error));
+                    msg.channel.send('Deleted ' + msgar.length + ' messages from **' + msg.mentions.users.first().username + '** under request of <@' + msg.author.id + '>')
+                        .then(msg2 => setTimeout(() => { msg2.delete(); }, 5000));
+				});
             } else {
                 msg.channel.send('Please specify a number!');
             }
