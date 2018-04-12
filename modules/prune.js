7b6c307185aa0667330a6f3394b4fb1e6df9c124
Michael Cao <mike.x.cao@gmail.com>
2018-04-12 11:10:27 -0400

    Update prune.js

--- modules/prune.js
+++ modules/prune.js
@@ -9,25 +9,31 @@ module.exports = {
         if (msg.member.hasPermission('MANAGE_MESSAGES') || msg.author.id === require('../config.json').owner) {
             var num = msg.content;
             if (!isNaN(num)) {
-                msg.channel.fetchMessages({ limit: num })
+                msg.channel.fetchMessages({
+                        limit: num
+                    })
                     .then(messages => msg.channel.bulkDelete(messages))
                     .catch(msg.channel.bulkDelete);
 
                 msg.channel.send('Deleted ' + num + ' messages under request of <@' + msg.author.id + '>')
-                    .then(msg2 => setTimeout(() => { msg2.delete(); }, 5000));
+                    .then(msg2 => setTimeout(() => {
+                        msg2.delete();
+                    }, 5000));
             } else if (msg.mentions.users.first()) {
                 var amount = num.split(" ")[1] || null;
                 msg.channel.fetchMessages({
-					limit: 100
-				})
-				.then(messages => {
-					msgar = messages.array();
-					msgar = msgar.filter(msg => msg.author.id === msg.mentions.users.first().id);
-					if (amount && !isNaN(amount)) msgar.length = amount + 1;
-					msgar.map(msg => msg.delete().catch(console.error));
-                    msg.channel.send('Deleted ' + msgar.length + ' messages from **' + msg.mentions.users.first().username + '** under request of <@' + msg.author.id + '>')
-                        .then(msg2 => setTimeout(() => { msg2.delete(); }, 5000));
-				});
+                        limit: 100
+                    })
+                    .then(messages => {
+                        msgar = messages.array();
+                        msgar = msgar.filter(msg2 => msg2.author.id === msg.mentions.users.first().id);
+                        if (amount && !isNaN(amount)) msgar.length = amount + 1;
+                        msgar.map(msg => msg.delete().catch(console.error));
+                        msg.channel.send('Deleted ' + msgar.length + ' messages from **' + msg.mentions.users.first().username + '** under request of <@' + msg.author.id + '>')
+                            .then(msg2 => setTimeout(() => {
+                                msg2.delete();
+                            }, 5000));
+                    });
             } else {
                 msg.channel.send('Please specify a number!');
             }
