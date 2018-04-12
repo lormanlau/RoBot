56890f0931119f15d66f78cb82a81a0c0ea2c292
Michael Cao <mike.x.cao@gmail.com>
2018-04-12 11:29:47 -0400

    more linting errors

--- modules/prune.js
+++ modules/prune.js
@@ -12,41 +12,41 @@ module.exports = {
                 msg.channel.fetchMessages({
                     limit: num,
                 })
-                .then(messages => msg.channel.bulkDelete(messages))
-                .catch(msg.channel.bulkDelete);
+                    .then(messages => msg.channel.bulkDelete(messages))
+                    .catch(msg.channel.bulkDelete);
 
                 msg.channel.send('Deleted ' + num + ' messages under request of <@' + msg.author.id + '>')
-                .then(msg2 => setTimeout(() => {
-                    msg2.delete();
-                }, 5000));
+                    .then(msg2 => setTimeout(() => {
+                        msg2.delete();
+                    }, 5000));
             } else if (msg.mentions.users.first()) {
-                var amount = num.split(" ")[1] || null;
+                var amount = num.split(' ')[1] || null;
                 msg.channel.fetchMessages({
-                    limit: 100
+                    limit: 100,
                 })
-                .then(messages => {
-                    var msgar = messages.array();
-                    msgar = msgar.filter(msg2 => msg2.author.id === msg.mentions.users.first().id);
-                    if (amount && !isNaN(amount)) msgar = msgar.slice(0, amount);
-                    msgar.map(msg => msg.delete().catch(console.error));
-                    msg.channel.send('Deleted ' + msgar.length + ' messages from **' + msg.mentions.users.first().username + '** under request of <@' + msg.author.id + '>')
-                        .then(msg2 => setTimeout(() => {
-                            msg2.delete();
-                        }, 5000));
-                });
-            } else if (num == "bots") {
+                    .then(messages => {
+                        var msgar = messages.array();
+                        msgar = msgar.filter(msg2 => msg2.author.id === msg.mentions.users.first().id);
+                        if (amount && !isNaN(amount)) msgar = msgar.slice(0, amount);
+                        msgar.map(m => m.delete().catch(console.error));
+                        msg.channel.send('Deleted ' + msgar.length + ' messages from **' + msg.mentions.users.first().username + '** under request of <@' + msg.author.id + '>')
+                            .then(msg2 => setTimeout(() => {
+                                msg2.delete();
+                            }, 5000));
+                    });
+            } else if (num === 'bots') {
                 msg.channel.fetchMessages({
-                    limit: 100
+                    limit: 100,
                 })
-                .then(messages => {
-                    var msgar = messages.array();
-                    msgar = msgar.filter(msg => msg.author.bot);
-                    msgar.map(msg => msg.delete().catch(console.error));
-                    msg.channel.send('Deleted ' + msgar.length + ' messages from **bots** under request of <@' + msg.author.id + '>')
-                        .then(msg2 => setTimeout(() => {
-                            msg2.delete();
-                        }, 5000));
-                });
+                    .then(messages => {
+                        var msgar = messages.array();
+                        msgar = msgar.filter(msg => msg.author.bot);
+                        msgar.map(m => m.delete().catch(console.error));
+                        msg.channel.send('Deleted ' + msgar.length + ' messages from **bots** under request of <@' + msg.author.id + '>')
+                            .then(msg2 => setTimeout(() => {
+                                msg2.delete();
+                            }, 5000));
+                    });
             } else {
                 msg.channel.send('Please specify a number, mention a user, mention a user and specify a number, or use the bots argument!');
             }
