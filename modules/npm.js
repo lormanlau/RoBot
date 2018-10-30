ea96c18e18a4333a316406b3be585f62eee28939
Michael Cao <mike.x.cao@gmail.com>
2018-10-30 16:45:15 -0400

    hot fix

--- modules/npm.js
+++ modules/npm.js
@@ -6,23 +6,24 @@ module.exports = {
     help: 'Gives you information about an npm package',
     main: function (bot, msg) {
         var info = require("package-info"),
-            { MessageEmbed } = require("discord.js");
+            { RichEmbed } = require("discord.js");
 
-        var pkg = await info(args.join(" "));
-        msg.channel.send({
-            embed: new MessageEmbed()
-                .setAuthor(
-                    pkg.name,
-                    "https://authy.com/wp-content/uploads/npm-logo.png",
-                    `https://npmjs.com/package/${pkg.name}`
-                )
-                .setDescription(pkg.description)
-                .setFooter("Powered by package-info")
-                .setTimestamp()
-                .setColor(msg.guild.me.displayHexColor)
-                .addField("Author", pkg.author, true)
-                .addField("Version", pkg.version, true)
-                .addField("License", pkg.license || "None", true)
-        });
+        info(msg.args.join(" ")).then(pkg => {
+            msg.channel.send({
+                embed: new RichEmbed()
+                    .setAuthor(
+                        pkg.name,
+                        "https://authy.com/wp-content/uploads/npm-logo.png",
+                        `https://npmjs.com/package/${pkg.name}`
+                    )
+                    .setDescription(pkg.description)
+                    .setFooter("Powered by package-info")
+                    .setTimestamp()
+                    .setColor(msg.guild.me.displayHexColor)
+                    .addField("Author", pkg.author, true)
+                    .addField("Version", pkg.version, true)
+                    .addField("License", pkg.license || "None", true)
+            });
+        })
     },
 };
