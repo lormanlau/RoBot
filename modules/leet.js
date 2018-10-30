0b3b1f575302c97d777cba4a27bab827bef6bc80
Michael Cao <mike.x.cao@gmail.com>
2018-10-30 16:43:07 -0400

    hot fix

--- modules/leet.js
+++ modules/leet.js
@@ -1,35 +1,35 @@
 module.exports = {
-    name: 'leet',
-    type: 'fun',
-    usage: 'leet <message>',
-    permission: 1,
-    help: 'Returns the input, but in 1337speak.',
-    main: function(bot, message) {
-        message.channel.send(leet(message.content) || 'Error');
+  name: "leet",
+  type: "fun",
+  usage: "leet <message>",
+  permission: 1,
+  help: "Returns the input, but in 1337speak.",
+  main: function(bot, message) {
+    message.channel.send(leet(message.content) || "Error");
 
-        function leet(str) {
-            var map = {
-                a: '4',
-                e: '3',
-                f: 'ph',
-                g: '9',
-                l: '1',
-                o: '0',
-                s: '5',
-                t: '7',
-                y: `\`/`,
-            };
-            if (str === null || typeof str === 'undefined') {
-                str = null;
-            }
+    function leet(str) {
+      var map = {
+        a: "4",
+        e: "3",
+        f: "ph",
+        g: "9",
+        l: "1",
+        o: "0",
+        s: "5",
+        t: "7",
+        y: `\`/`
+      };
+      if (str === null || typeof str === "undefined") {
+        str = null;
+      }
 
-            var newStr = '';
+      var newStr = "";
 
-            for (var i = 0; i < str.length; i++) {
-                newStr += map[str[i].toLowerCase()] || str[i];
-            }
+      for (var i = 0; i < str.length; i++) {
+        newStr += map[str[i].toLowerCase()] || str[i];
+      }
 
-            return newStr;
-        }
-    },
+      return newStr;
+    }
+  }
 };
--- modules/npm.js
+++ modules/npm.js
@@ -4,23 +4,25 @@ module.exports = {
     usage: 'npm <package-name>',
     permission: 1,
     help: 'Gives you information about an npm package',
-    main: function(bot, msg) {
-        var npmPackage = require('npm-package-info'), { RichEmbed } = require('discord.js');
+    main: function (bot, msg) {
+        var info = require("package-info"),
+            { MessageEmbed } = require("discord.js");
 
-        npmPackage(msg.content, (err, pkg) => {
-            if (err) msg.channel.send('Package not found.');
-            msg.channel.send({
-                embed: new RichEmbed()
-                    .setAuthor(pkg.name, 'https://authy.com/wp-content/uploads/npm-logo.png', `https://npmjs.com/package/${pkg.name}`)
-                    .setDescription(pkg.description)
-                    .setFooter('Powered by npm-package-info')
-                    .setTimestamp()
-                    .setColor(msg.guild.me.displayHexColor)
-                    .addField('Author', pkg.author ? pkg.author.name : 'None', true)
-                    .addField('Version', pkg['dist-tags'].latest, true)
-                    .addField('License', pkg.license || 'None', true)
-                    .addField('Keywords', pkg.keywords ? pkg.keywords.join(', ') : 'None', true),
-            });
+        var pkg = await info(args.join(" "));
+        msg.channel.send({
+            embed: new MessageEmbed()
+                .setAuthor(
+                    pkg.name,
+                    "https://authy.com/wp-content/uploads/npm-logo.png",
+                    `https://npmjs.com/package/${pkg.name}`
+                )
+                .setDescription(pkg.description)
+                .setFooter("Powered by package-info")
+                .setTimestamp()
+                .setColor(msg.guild.me.displayHexColor)
+                .addField("Author", pkg.author, true)
+                .addField("Version", pkg.version, true)
+                .addField("License", pkg.license || "None", true)
         });
     },
 };
